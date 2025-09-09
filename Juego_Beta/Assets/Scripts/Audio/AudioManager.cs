f4a9e92fca5099d137bfcba2d370877514c1526a
Luis Xavier <luisxavierxd@gmail.com>
2025-09-09 12:41:58 -0600

    Audio Manager y canciones completadas

---
+++ Juego_Beta/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,23 @@
+
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    [Header("----------Audio Source------------")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    [Header("----------Audio Clip------------")]
+    public AudioClip background;
+    public AudioClip casa;    
+    public AudioClip ataque;
+    public AudioClip victoria;
+    public AudioClip muerte;
+    public AudioClip puerta;
+    public AudioClip daño;
+    public AudioClip item;
+    public AudioClip batalla;
+
+
+}
